5443de3d53004e730f13d9643837562ea5f33434
Aniketrana <ranaannu93@gmail.com>
2020-07-27 15:09:25 +0530

    Add project files.

---
+++ FiltersInMVC/Controllers/ErrController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FiltersInMVC.Controllers
+{
+    [HandleError]
+    public class ErrController : Controller
+    {
+        public ActionResult Index()
+        {
+            throw new Exception("This is a error");
+        }
+        public ActionResult About() 
+        {
+            throw new Exception("This is a error");
+        }
+    }
+}
